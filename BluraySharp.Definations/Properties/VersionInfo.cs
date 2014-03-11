4f95d0290e1c0c1e50cb69f241278f3ac75d26ec
Subelf <adm@subelf.net>
2014-03-11 20:43:42 +0800

    #Version 0.2.2

--- BluraySharp.Definations/Properties/VersionInfo.cs
+++ BluraySharp.Definations/Properties/VersionInfo.cs
@@ -8,8 +8,8 @@ internal static partial class VersionInfo
 	private const string D = ".";
 
 	private const string M = "0";
-	private const string S = "0";
-	private const string I = "1";
+	private const string S = "2";
+	private const string I = "2";
 
 	public const string Value = M + D + S + D + I + D + R;
 }
