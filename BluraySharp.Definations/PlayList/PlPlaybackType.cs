3be09610ef4fb7f05cd6d0e29b2d041b46122d1f
Subelf <adm@subelf.net>
2015-05-09 10:15:10 +0800

    *Migrated to bitbucket

--- BluraySharp.Definations/PlayList/PlPlaybackType.cs
+++ BluraySharp.Definations/PlayList/PlPlaybackType.cs
@@ -4,7 +4,7 @@
  * 
  * This is a C# library project aimed to parse or compose BDMV files.
  * 
- * Maintained at Google Code (https://code.google.com/p/bluray-sharp/)
+ * Maintained at BitBucket (https://bitbucket.org/subelf/bluraysharp)
  * Released under the terms of LGPL (http://www.gnu.org/licenses/lgpl.html).
  *
  * Mar. 2014, adm@subelf.net
